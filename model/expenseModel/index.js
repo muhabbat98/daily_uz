8bc5e71764fbd3b8c4b798c7814e1a2483fd0ed5
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-11-30 15:59:29 +0500

    select

--- model/expenseModel/index.js
+++ model/expenseModel/index.js
@@ -4,7 +4,9 @@ const CREATE_EXPENSE = "INSERT INTO expense(expense_name, image_id) VALUES($1, $
 const CREATE_EXPENSE_ITEM = "INSERT INTO expense_item(expense_id, item_name, cost, buyed_at ) VALUES($1, $2, $3, $4)RETURNING *"
 const CREATE_IMAGE ="INSERT INTO images(path) VALUES ($1) RETURNING *"
 
-const EXPENSES = "SELECT * FROM expense e LEFT JOIN expense_item i ON e.expense_id= i.expense_id"
+// const EXPENSES = "SELECT * FROM expense e LEFT JOIN expense_item i ON e.expense_id= i.expense_id"
+const EXPENSES = "SELECT * FROM expense"
+
 const EXPENSE_ITEMS = "SELECT * FROM expense_item"
 const EXPENSE_ITEM = "SELECT * FROM expense_item WHERE expense_id=$1"
 
