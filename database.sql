f4a27b8f893e0f08118bb51f81a7002dac045423
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-12-09 11:08:46 +0500

    primary key

--- database.sql
+++ database.sql
@@ -5,18 +5,18 @@ create extension pgcrypto;
 -- char(n) = character(n) if shorter then declered length space-padded
 
 create table users(
-    user_id serial not null unique,
+    user_id serial not null unique primary key,
     username varchar(16) unique not null,
     password varchar(72) not null
 );
-
+DROP TABLE IF EXISTS images CASCADE;
 create table images(
-    image_id serial not null unique,
+    image_id serial not null unique primary key,
     path varchar not null
 );
 DROP TABLE IF EXISTS expense CASCADE;
 create table expense (
-    expense_id serial not null unique,
+    expense_id serial not null unique primary key,
     expense_name varchar(32) unique not null,
     user_id int references users(user_id),
     image_id int references images(image_id)
@@ -25,7 +25,7 @@ create table expense (
 -- numeric type slow but we can define with items with exact numeric(precision, scale)
 DROP TABLE IF EXISTS expense_item;
 create table expense_item (
-    expense_item_id serial not null unique,
+    expense_item_id serial not null unique primary key,
     expense_id int references expense(expense_id),
     item_name varchar(64)not null,
     cost numeric(6,3) not null,
