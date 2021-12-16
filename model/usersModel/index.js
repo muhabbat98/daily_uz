2cfb0f44fee81dcdc92c15ecbe1fa8ccebda1006
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-12-16 17:02:16 +0500

    password salt

--- model/usersModel/index.js
+++ model/usersModel/index.js
@@ -3,7 +3,7 @@ const { ModuleSingle, ModuleArr } = require( '../../module/pool' )
 const CREATE_USER = "INSERT INTO users(username, password) VALUES($1, crypt($2,gen_salt('bf')))RETURNING user_id, username"
 const USERS = "SELECT username, user_id FROM users"
 const DELETE_USER = "DELETE FROM users WHERE user_id=$1"
-const IS_USER = "SELECT * FROM users WHERE username=$1 AND password=$2"
+const IS_USER = "SELECT * FROM users WHERE username=$1 AND password = crypt('123',password)"
 
 const addUser = (username, password) => ModuleSingle( CREATE_USER, username, password )
 const selectUsers = () => ModuleArr( USERS )
