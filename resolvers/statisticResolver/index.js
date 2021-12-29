e2460a7df91bfd30d83e8053300f7c5b1c6c1765
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-12-29 17:44:12 +0500

    sub fields

--- resolvers/statisticResolver/index.js
+++ resolvers/statisticResolver/index.js
@@ -3,14 +3,15 @@ const { token, check } = require("../../settings/jwt");
 const {monthlyExpenses} = require("../../model/statisticalModel")
 const resolvers = {
   Query: {
-    monthlyExpenses: async(_,{month}) =>{
+    monthlyExpenses: async(_,{month},{token}) =>{
       const expense = await monthlyExpenses()
-      console.log(month, expense)
+      console.log(month, expense, token)
       return expense
     }
   },
   MonthExpense: {
-    id:global=>console.log(global)
+    id: global => global.expense_id,
+    name:global=>global.expense_name,
   }
 };
 
